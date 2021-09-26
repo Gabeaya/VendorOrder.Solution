64d52f1aa26ebecb3e49130a5eda773feebd3c66
Gabriel Ayala <gabeayala100@gmail.com>
2021-09-26 14:15:34 -0700

    Test that our class will get its title property value.

--- VendorOrder.Tests/ModelTests/OrderTests.cs
+++ VendorOrder.Tests/ModelTests/OrderTests.cs
@@ -10,8 +10,20 @@ namespace VendorOrder.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order();
+      Order newOrder = new Order("test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string title = "Pan Dulces";
+      Order newOrder = new Order(title);
+      //Act
+      string result = newOrder.Title;
+
+      //Assert
+      Assert.AreEqual(title, result);
     }
   }
 }
