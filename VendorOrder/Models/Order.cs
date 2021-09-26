e010e96a187f71d1a0f6df49f518af6850d6fd18
Gabriel Ayala <gabeayala100@gmail.com>
2021-09-26 14:42:58 -0700

    Add the constructor.

--- VendorOrder/Models/Order.cs
+++ VendorOrder/Models/Order.cs
@@ -5,9 +5,17 @@ namespace VendorOrder.Models
   public class Order
   {
     public string Title { get; }
-    public Order(string title)
+    public string Description { get; set; }
+    public string Price { get; set; }
+    public string Date { get; set; }
+    private static List<Order> _instances = new List<Order> {};
+    //Constructor
+    public Order(string title, string description, string price, string date)
     {
       Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
     }
   }
 }
