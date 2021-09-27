0ea2bc05b5518fa789f89d648201f3e93895dadc
Gabriel Ayala <gabeayala100@gmail.com>
2021-09-27 16:13:20 -0700

    Add conditionals for the vendor display and a home link.

---
+++ VendorOrder/Controllers/VendorsController.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorOrder.Models;
+
+namespace VendorOrder.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+    [HttpGet("/categories/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+  }
+}
