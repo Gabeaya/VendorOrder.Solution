00c26c50fae4679b79bfe34053ad659854f625d5
Gabriel Ayala <gabeayala100@gmail.com>
2021-09-26 17:30:14 -0700

    Update line 24.

---
+++ VendorOrder/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorOrder.Models;
+using System.Collections.Generic;
+
+namespace VendorOrder.Controllers
+{
+  public class OrdersController : Controller
+  {
+    [HttpGet("/orders")]
+    public ActionResult Index()
+    {
+      List<Order> allOrders = Order.GetAll();
+      return View(allOrders);
+    }
+    [HttpGet("/orders/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+    [HttpPost("/orders")]
+    public ActionResult Create(string title, string description, string price, string date)
+    {
+      Order myOrder = new Order(title, description, price, date);
+      return RedirectToAction("Index");
+    }
+  }
+}
