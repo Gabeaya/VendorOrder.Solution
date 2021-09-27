7133b6bb0d548250146307a0ea030d7c88aefdcf
Gabriel Ayala <gabeayala100@gmail.com>
2021-09-27 15:35:51 -0700

    Update tests that were missing a description arguement.

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -15,30 +15,35 @@ namespace VendorOrder.TestTools
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("test vendor");
+      Vendor newVendor = new Vendor("test vendor", "test description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
-    }
-    [TestMethod]
-    public void GetName_ReturnsName_String()
-    {
-      //Arrange
-      string name = "Test Vendor";
-      Vendor newVendor = new Vendor(name);
-      //Act
-      string result = newVendor.Name;
-      //Assert
-      Assert.AreEqual(name, result);
     }
     [TestMethod] public void GetId_ReturnsVendorId_Int()
     {
       //Arrange
       string name = "Test Vendor";
-      Vendor newVendor = new Vendor(name);
+      string description = "Test Description";
+      Vendor newVendor = new Vendor(name, description);
       //Act
       int result = newVendor.Id;
       //Assert
       Assert.AreEqual(1, result);
-
+    }
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      //Arrange
+      string name1 = "test";
+      string name2 = "test2";
+      string description1 = "this is test";
+      string description2 = "this is test 2";
+      Vendor newVendor1 = new Vendor(name1, description1);
+      Vendor newVendor2 = new Vendor(name2, description2);
+      List<Vendor> newList = new List<Vendor> {newVendor1, newVendor2};
+      //Act
+      List<Vendor> result = Vendor.GetAll();
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
