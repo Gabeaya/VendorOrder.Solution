c4a7dc39ce5a2922620c4d6fafa75aa144a1e130
Gabriel Ayala <gabeayala100@gmail.com>
2021-09-30 12:37:28 -0700

    Change the singular variables to plural.

--- VendorOrder/Controllers/VendorsController.cs
+++ VendorOrder/Controllers/VendorsController.cs
@@ -30,21 +30,21 @@ namespace VendorOrder.Controllers
       Dictionary<string, object> model = new Dictionary<string, object>();
       Vendor selectedVendor = Vendor.Find(id);
       List<Order> vendorOrders = selectedVendor.Orders;
-      model.Add("vendors", selectedVendor);
+      model.Add("vendor", selectedVendor);
       model.Add("orders", vendorOrders);
       return View(model);
     }
     //this one creates new Orders within the given Vendor, not new Vendors
     [HttpPost("/vendors/{vendorId}/orders")]
-    public ActionResult Create(int vendorId, string title, string description, string price, string date)
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, string orderPrice, string orderDate)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
       Vendor foundVendor = Vendor.Find(vendorId);
-      Order newOrder = new Order(title, description, price, date);
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
       foundVendor.AddOrder(newOrder);
       List<Order> vendorOrders = foundVendor.Orders;
       model.Add("orders", vendorOrders);
-      model.Add("vendors", foundVendor);
+      model.Add("vendor", foundVendor);
       return View("Show", model);
     }
   }
