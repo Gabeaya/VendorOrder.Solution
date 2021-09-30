0785b1999b33b7099fc26237f829b3ce423af3b1
Gabriel Ayala <gabeayala100@gmail.com>
2021-09-30 12:33:15 -0700

    Change the words plural.

--- VendorOrder/Controllers/OrdersController.cs
+++ VendorOrder/Controllers/OrdersController.cs
@@ -18,8 +18,8 @@ namespace VendorOrder.Controllers
       Order order = Order.Find(orderId);
       Vendor vendor = Vendor.Find(vendorId);
       Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("orders", order);
-      model.Add("vendors", vendor);
+      model.Add("order", order);
+      model.Add("vendor", vendor);
       return View(model);
     }
   }
