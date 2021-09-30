6861942695af68d71eb63fd167b4db1ad6205572
Gabriel Ayala <gabeayala100@gmail.com>
2021-09-30 12:45:31 -0700

    Update the description to have the vendor keyword.

--- VendorOrder/Models/Vendor.cs
+++ VendorOrder/Models/Vendor.cs
@@ -6,14 +6,14 @@ namespace VendorOrder.Models
   {
     private static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set; }
-    public string Description { get; set; }
+    public string VendorDescription { get; set; }
     public int Id { get; }
     public List<Order> Orders { get; set; }
   
     public Vendor(string vendorName, string vendorDescription)
     {
       Name = vendorName;
-      Description = vendorDescription;
+      VendorDescription = vendorDescription;
       _instances.Add(this);
       Id = _instances.Count;
       Orders = new List<Order>{};
@@ -28,7 +28,7 @@ namespace VendorOrder.Models
     }
     public static Vendor Find(int searchId)
     {
-      return _instances[searchId-1];
+      return _instances[searchId - 1];
     }
     public void AddOrder(Order order)
     {
